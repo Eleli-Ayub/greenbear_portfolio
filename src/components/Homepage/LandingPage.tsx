d0a82a433aaab2f9a051801ab88f9bea3c5f99ab
Eleli-Ayub <lesosioayub@gmail.com>
2023-01-06 12:34:41 +0300

    done with landing page

--- src/components/Homepage/LandingPage.tsx
+++ src/components/Homepage/LandingPage.tsx
@@ -11,53 +11,59 @@ import { SiGmail } from "react-icons/si";
 
 function LandingPage() {
   return (
-    <div className="relative px-20 flex flex-row h-screen justify-between pt-6 pb-20 bg-green-50">
-      <section className=" flex flex-col justify-start items-center">
+    <div className="relative px-32 flex flex-row h-screen justify-between pt-6 pb-20 bg-green-50">
+      <section className=" flex flex-col justify-start items-center text-gray-500 w-1/2">
         <div className=" flex flex-row items-center gap-4 self-start mb-32">
           <img
             className="h-16 object-contain"
             src={require("../../Assets/Icons/logo.png")}
             alt=""
           />
-          <h2 className=" font-bold text-2xl text-gray-500">GreenBear</h2>
+          <h2 className=" font-bold text-2xl ">GreenBear</h2>
         </div>
         <section className="flex flex-col gap-5">
-          <h4>Best Agency In Africa</h4>
-          <h1>Digital Agency</h1>
-          <h1>& Solutions</h1>
-          <p>
-            Quickly grow your business & boost up sale with professiona startup
+          <h4 className="text-2xl text-gray-600 font-bold">
+            Best Agency In <span className="text-teal-500">Africa</span>
+          </h4>
+          <h1 className="font-bold text-5xl text-gray-700">Digital Agency</h1>
+          <h1 className="font-light text-4xl text-gray-700">& Solutions</h1>
+          <p className=" text-xl text-gray-500">
+            Quickly grow your business & boost up sale with professional startup
+            by collaborating witht the best developers and entreprenual minds in
+            the market.
           </p>
         </section>
-        <div>
-          <button>
+        <div className="mt-10 flex flex-row gap-8 ">
+          <button className="bg-green-500 rounded-20 flex flex-row items-center gap-3 px-4 rounded-xxl text-white h-10 font-bold border-2 border-green-500 hover:text-gray-500 hover:bg-transparent	 hover:border-red hover:border-2">
             Get Started
-            <FaGreaterThan />
+            <FaGreaterThan className="text-gray-700 hover:text-gray-500" />
           </button>
-          <button>
+          <button className="bg-green-100 rounded-20 flex flex-row items-center gap-3 px-4 rounded-xxl text-black font-bold h-10 border-dashed border-2 border-gray-300 hover:border-gray-500">
             Live Demos
-            <AiFillPlayCircle />
+            <AiFillPlayCircle className="text-2xl" />
           </button>
         </div>
       </section>
-      <section>
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">Our Work</a>
-          </li>
-          <li>
-            <a href="">Services</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Contact Us</a>
-          </li>
-        </ul>
+      <section className="relative flex flex-row justify-center w-1/2 text-white font-light text-xl bg-homeBg bg-center bg-cover ">
+        <div className="absolute h-full w-full bg-green-200 bg-opacity-10 pt-10 flex flex-row justify-start pl-4">
+          <ul className="flex flex-row gap-4 ">
+            <li className="rounded-xxl hover:bg-green-100 hover:text-black h-10 w-24 justify-center flex flex-row items-center duration-300 ease-linear">
+              <a href="">Home</a>
+            </li>
+            <li className="rounded-xxl hover:bg-green-100 hover:text-black h-10 w-24 justify-center flex flex-row items-center duration-300 ease-linear">
+              <a href="">Our Work</a>
+            </li>
+            <li className="rounded-xxl hover:bg-green-100 hover:text-black h-10 w-24 justify-center flex flex-row items-center duration-300 ease-linear">
+              <a href="">Services</a>
+            </li>
+            <li className="rounded-xxl hover:bg-green-100 hover:text-black h-10 w-24 justify-center flex flex-row items-center duration-300 ease-linear">
+              <a href="">About</a>
+            </li>
+            <li className="rounded-xxl hover:bg-green-100 hover:text-black h-10 w-24 justify-center flex flex-row items-center duration-300 ease-linear">
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
       </section>
       <section className="absolute hidden">
         <ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        none: "0",
+        sm: "0.125rem",
+        DEFAULT: "0.25rem",
+        DEFAULT: "4px",
+        md: "0.375rem",
+        lg: "0.5rem",
+        xxl: "1.5rem",
+        full: "9999px",
+        large: "12px",
+      },
+      backgroundImage: {
+        homeBg: "url('./Assets/Images/Homebg.jpg')",
+      },
+    },
   },
   plugins: [],
 };
